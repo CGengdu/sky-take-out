fafd89bbb4772c12b3ad8a8d2f452ede9f846f7c
gengduc <gengduc@qq.com>
2023-11-01 19:13:50 +0800

    套餐业务（更新套餐，起售套餐）开发

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -3,18 +3,21 @@ package com.sky.service;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
 
 import java.util.List;
 
 public interface SetmealService {
     /**
      * 新增套餐
+     *
      * @param setmealDTO
      */
     void saveWithDish(SetmealDTO setmealDTO);
 
     /**
      * 套餐分页查询
+     *
      * @param setmealPageQueryDTO
      * @return
      */
@@ -22,7 +25,30 @@ public interface SetmealService {
 
     /**
      * 套餐批量删除
+     *
      * @param ids
      */
     void delateBatch(List<Long> ids);
+
+    /**
+     * 根据id查询套餐和套餐菜品关系
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 修改套餐
+     *
+     * @param setmealDTO
+     */
+    void update(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐起售、停售
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+
 }
